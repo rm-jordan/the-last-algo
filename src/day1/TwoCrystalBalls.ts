71abe60f369a562ef77a6be43472ec1e7525aaa1
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-11-13 09:40:26 -0500

    crystal ball start

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,12 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  // we want to jump by the sqrt of N
+
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  
+
+
 }
+
+// https://frontendmasters.com/courses/algorithms/two-crystal-balls-problem/
+// https://frontendmasters.com/courses/algorithms/implementing-two-crystal-balls/
